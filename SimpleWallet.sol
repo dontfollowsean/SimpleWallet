f5f9103728d339b91765ccc924958274f996ebb6
Sean Wilkinson <swilkinson@smarttermlms.com>
2018-02-14 23:12:19 -0500

    Send funds if allowed

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -3,5 +3,29 @@ pragma solidity ^0.4.0;
 import "github.com/ethereum/solidity/std/mortal.sol";
 
 contract SimpleWallet is mortal {
-    
+    mapping(address => Permission) myAddressMapping;
+
+    struct Permission {
+        bool isAllowed;
+        uint maxTransferAmount;
+    }
+
+    function addAddressToSendersList(address permitted, uint maxTransferAmount) {
+        myAddressMapping[permitted] = Permission(true, maxTransferAmount);
+    }
+
+    function sendFunds(address reciever, uint amountInWei) {
+        if (myAddressMapping[msg.sender].isAllowed) {
+            if (myAddressMapping[msg.sender].maxTransferAmount <= amountInWei) {
+                bool isSuccessful = receiver.send(amountInWei);
+                if (!isSuccessful) {
+                    throw;
+                }
+            }
+        }
+    }
+
+    function () payable {
+        
+    }
 }
